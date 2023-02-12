5030d7c081370972ebf873006a485921a44bea18
Shohan Rahman <shohan.rahman@chaldal.net>
2023-02-12 16:24:40 +0600

    Started editing

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,65 @@
 import logo from './logo.svg';
+import Gallery from './Gallery.js';
+// import {Profile} from './Gallery.js';
 import './App.css';
+
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+      Click me
+    </button>
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+const products = [
+  {title: 'product1'},
+  {title: 'product2'}
+]
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <header className="New React App">
+        
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>the src/App.js</code> and save to reload.
         </p>
+
+        < MyButton />
+
+        < Gallery />
+
+        < Profile />
+
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +71,14 @@ function App() {
       </header>
     </div>
   );
+
+
 }
+
+
+
+
+
+
 
 export default App;
